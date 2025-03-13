da1ad7024d82c90c4a444082297b899372655de4
apark00 <park.ant@northeastern.edu>
2025-03-13 03:52:17 -0400

    a

--- src/Kambaz/Account/Profile.tsx
+++ src/Kambaz/Account/Profile.tsx
@@ -1,5 +1,5 @@
 import { FormControl, Button } from "react-bootstrap";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { setCurrentUser } from "./reducer";
