9d50b1c69b38c23e4ca84dfcbaa5f9327daf12f1
apark00 <park.ant@northeastern.edu>
2025-03-13 00:30:26 -0400

    courses

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -1,5 +1,4 @@
 import CourseNavigation from "./Navigation";
-import { courses } from "../Database";
 import { Navigate, Route, Routes, useParams, useLocation } from "react-router";
 import Modules from "./Modules";
 import Home from "./Home";
@@ -7,7 +6,7 @@ import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor"
 import PeopleTable from "./People/Table";
 import { FaAlignJustify } from "react-icons/fa";
-export default function Courses() {
+export default function Courses({ courses }: { courses: any[]; }) {
     const { cid } = useParams();
     const course = courses.find((course) => course._id === cid);
     const { pathname } = useLocation();
